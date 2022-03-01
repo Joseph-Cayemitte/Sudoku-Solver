88052aaf6d43932bf6408cc55e218805148c3823
Joseph Cayemitte <josephcayemitte@gmail.com>
2022-03-01 15:06:40 -0500

    Base Project

---
+++ app/src/main/java/com/example/sudokusolverapp/SudokuBoard.java
@@ -0,0 +1,47 @@
+package com.example.sudokusolverapp;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SudokuBoard extends View {
+    private  final int boardColor;
+    private final Paint boardColorPaint = new Paint();
+
+    public SudokuBoard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SudokuBoard,
+                0, 0);
+
+        try{
+            boardColor = a.getInteger(R.styleable.SudokuBoard_boardColor, 0);
+        }finally {
+            a.recycle();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int width, int height){
+        super.onMeasure(width, height);
+
+        int dimension = Math.min(this.getWidth(), this.getHeight());
+
+        setMeasuredDimension(dimension, dimension);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        boardColorPaint.setStyle(Paint.Style.STROKE);
+        boardColorPaint.setStrokeWidth(16);
+        boardColorPaint.setColor(boardColor);
+        boardColorPaint.setAntiAlias(true);
+
+        canvas.drawRect(0, 0, getWidth(),getHeight(), boardColorPaint);
+    }
+}
